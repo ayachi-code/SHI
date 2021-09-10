4bd9886a3eb5830189c6019a2e5e8ae9349b525f
Bilal ayachi <bilal.ayachi@hetmml.nl>
2021-09-10 15:59:56 +0200

    convert to cipher functie gemaakt

--- encrpytion.c
+++ encrpytion.c
@@ -7,6 +7,12 @@
  * This encryption is made by Bilal
 */
 
+char* convert_to_cipher(char* message) {
+    return message;
+}
+
+
 int main(int argc, char *argv[]) {
+    convert_to_cipher(argv[1]);
     return 0;
 }
