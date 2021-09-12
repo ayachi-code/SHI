cb6630c47d563a91848b427f818cd539722a3757
Bilal ayachi <bilal.ayachi@hetmml.nl>
2021-09-12 22:25:31 +0200

    convert hex func verwijderd

--- includes/encryption.h
+++
@@ -1,5 +0,0 @@
-/**
- * @returns returns the length of the message
- * @param message the message that will be encrypted
- **/
-char* convert_to_hex(char* message); 
--- src/encrpytion.c
+++ src/encrpytion.c
@@ -2,23 +2,6 @@
 #include "../includes/encryption.h"
 #include "../includes/usefull.h"
 
-/**
- * This encryption is made by Bilal
-**/
-
-char* convert_to_hex(char* message) {
-    int str_length = string_length(message);
-    char* hex_array = (char*) malloc(sizeof(char*) * str_length);
-    char temp_destination[3];
-    for (int i = 0; i < str_length; i++) {
-        sprintf(temp_destination,"%02x",message[i]);
-        hex_array[i] = (int)strtol(temp_destination, NULL, 16);
-    }
-    return hex_array;
-}
-
 int main(int argc, char *argv[]) {
-    char* hex_array = convert_to_hex("bilal");
-    free(hex_array);
     return 0;
 }
