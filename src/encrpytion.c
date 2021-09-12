1aebeb3cd56099de60af7a122d338f7f2dfdab4d
Bilal ayachi <bilal.ayachi@hetmml.nl>
2021-09-12 21:06:21 +0200

    convert text to hex

--- src/encrpytion.c
+++ src/encrpytion.c
@@ -8,12 +8,21 @@
 
 char* convert_to_hex(char* message) {
     int str_length = string_length(message);
-    printf("%d",str_length);
-    //char* hex_array = (char*) malloc(sizeof(char*) * 5);
-    return message;
+    char* hex_array = (char*) malloc(sizeof(char*) * str_length);
+    char temp_destination[3];
+    for (int i = 0; i < str_length; i++) {
+        sprintf(temp_destination,"%x",message[i]);
+        //hex_array[i] = *temp_destination;
+    }
+    // for (int i = 0; i < 6; i++) {
+    //     printf("%c \n",hex_array[i]);
+    // }
+    return hex_array;
 }
 
 int main(int argc, char *argv[]) {
     convert_to_hex("bilal");
+    // printf("%c",foo[1]);
+    //free(foo);
     return 0;
 }
