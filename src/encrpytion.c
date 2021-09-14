e0aebeb3df94b3851a279ab056280eb6e28975e8
Bilal ayachi <bilal.ayachi@hetmml.nl>
2021-09-14 10:06:44 +0200

    twee random nummers worden gegenereerd en er word gekeken als de twee nummers niet het zelfde zijn

--- src/encrpytion.c
+++ src/encrpytion.c
@@ -1,11 +1,23 @@
 #include <stdio.h>
+#include <time.h>
 #include "../includes/encryption.h"
 #include "../includes/usefull.h"
 
-int generate_publickey() {
+int generate_prime_number() {
+    srand(time(NULL));
+    int prime_number1 = rand() % 1000;
+    int prime_number2 = rand() % 1000;
+    while (prime_number2 == prime_number1) {
+        prime_number2 = rand() % 1000;
+    }
+    printf("%d %d \n",prime_number1,prime_number2);
+}
+
+int generate_publickey() { 
     return 1;
 }
 
 int main(int argc, char *argv[]) {
+    generate_prime_number();
     return 0;
 }
