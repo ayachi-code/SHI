1d59680b6a8bd37c38cacd9a9901f0afa69499c3
Bilal ayachi <bilal.ayachi@hetmml.nl>
2021-09-15 22:41:18 +0200

    m k waarden zijn nu wel goed

--- src/encrpytion.c
+++ src/encrpytion.c
@@ -7,17 +7,43 @@
 //cpp com
 bool is_prime(int number) {
     int k = 1;
-    double n = 53-1;
+    double n = number-1;
+    int a = 2;
+    double m = n;
     while (true) {
-        n /= pow(2,k);
-        if (fmod(n,1)==0) {
+        m = n/pow(2,k);
+        //printf("%f \n",m);
+        if (fmod(m,1)==0) {
             k++;
         } else {
-            n *= pow(2,k);
+            m *= 2;
             break;
         }
     }
-    printf("%f \n",n);
+    //printf("%f %d \n",n,k);
+    // int b0 = fmod(pow(2,n),number);
+    // if (b0 == fmod(1,number) || b0 == number-1) {
+    //     printf("Prime \n");
+    // }
+    // double b1 = 0;
+    // while (true) {
+    //     b1 = fmod(pow(b0,2),number);
+    //     printf("%f \n", b1);
+    //     if (b1 == fmod(1,number)) {
+    //         printf("geen prime \n");
+    //         break;
+    //     } else if (b1 == number-1) {
+    //         printf("PRIME \n");
+    //         break;
+    //     } else {
+    //         printf("volgende itteratie %f \n",b1);
+    //         break;
+    //     }
+    // }
+    // int b1 = 0;
+    // while (true) {
+    //     b1 = fmod(pow(),number);
+    // }
 }
 
 int generate_publickey() { 
@@ -25,6 +51,6 @@ int generate_publickey() {
 }
 
 int main(int argc, char *argv[]) {
-    is_prime(21);
+    is_prime(53);
     return 0;
 }
